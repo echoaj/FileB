34a8082a5fecba4ccfbecb0411229f90827b7283
echoaj <aljoslin13@yahoo.com>
2023-04-21 17:34:12 -0700

    first

---
+++ pages/views.py
@@ -0,0 +1,150 @@
+from django.shortcuts import render
+from django.conf import settings
+from .forms import InfoForm
+from .models import Info
+from pathlib import Path
+import shutil
+import environ
+import pyrebase
+import os
+
+
+# Limits file name size
+def chop_filename(filename, limit):
+    extension = str(filename).split('.').pop()
+    title = filename
+    if len(filename) > limit:
+        title = filename[0:limit]
+        title += "...." + extension
+    return title
+
+
+# Returns dictionary of general file information
+def dissect_file(filename: str) -> dict:
+    title = chop_filename(filename, limit=25)
+    split_name = str(filename).split('.')
+    extension = split_name.pop()
+    name = ".".join(split_name)
+    data = {"name": name,
+            "extension": extension,
+            "title": title}
+    return data
+
+
+def clear_media():
+    media_folder = Path(MEDIA_ROOT)
+    if media_folder.exists():
+        shutil.rmtree(MEDIA_ROOT)
+
+
+def db_save_text(text):
+    db_cur = Info()
+    if Info.objects.count() != 0:
+        db_last = Info.objects.last()
+        db_cur.file = db_last.file
+        db_cur.file_location = db_last.file_location
+    db_cur.text = text
+    db_cur.save()
+
+
+def handle_spaces(file_local_path):
+    # Fixes issue with names files with spaces.
+    # By default django renames those files with _
+    # Causes problem with retrieving that file
+    current_file_name = MEDIA_PATH + os.listdir(MEDIA_PATH)[0]
+    os.rename(current_file_name, file_local_path)
+
+
+def cloud_upload(file_name, file_local_path):
+    # 1st way of storing file on firebase
+    # storage.child(file_name).put(file=file_local_path)
+    blob = bucket.blob(file_name)
+    blob.upload_from_filename(file_local_path)  # Upload file to firebase
+    blob.make_public()  # Make file download url accessible
+    return blob.public_url
+
+
+# Create your views here.
+def home_view(request):
+    # global REFRESH_ON
+    # REFRESH_ON = False
+    text = ""
+    form = InfoForm()
+    file_data = {}
+    # Retrieve last text from database if db not empty
+    if Info.objects.count() > 0:
+        text = Info.objects.last().text
+        file_name = Info.objects.last().file.name
+        file_data = dissect_file(file_name)
+        file_url = Info.objects.last().file_location
+        file_data.update({"url": file_url})
+
+    if request.method == "POST":
+        if "text-clear-button" in request.POST:
+            text = ''
+            db_save_text(text)
+        elif "text-enter-button" in request.POST:
+            # REFRESH_ON = True
+            text = request.POST.get('user-input-text', False)
+            db_save_text(text)
+        elif "file-upload-button" in request.POST:
+            # REFRESH_ON = True
+            # InfoForm is a form that we defined in forms.py
+            form = InfoForm(request.POST, request.FILES)
+            if form.is_valid():
+                # Check if any items in media
+                if Info.objects.count() > 0:
+                    clear_media()
+                    db_last = Info.objects.last()
+                form.save()                                                 # save file to /media and to DB
+                file = request.FILES['file']                                # name of field we define in forms.py
+                file_name = file.name                                       # name of file
+                file_local_path = MEDIA_PATH + file_name                    # local path of file in /media
+                file_data = dissect_file(file_name)                         # gets file data from file.name
+                # Reverses django's default action of
+                # replacing ' ' with _ in file names
+                handle_spaces(file_local_path)
+                # Upload file to firebase and make url to it public
+                file_url = cloud_upload(file_name, file_local_path)
+                file_data.update({"url": file_url})
+                # Inject last DB record with previous text and file url
+                db_last_form = Info.objects.last()
+                if Info.objects.count() > 1:
+                    db_last_form.text = db_last.text
+                db_last_form.file_location = file_url
+                text = db_last_form.text
+                db_last_form.save()
+    return render(request, "home.html", {'text_info': text, "file": file_data, "form": form})
+
+"""
+def refresh_view(request):
+    print(request.method)
+    global REFRESH_ON
+    if request.method == "POST":
+        print("POSt create =============")
+        REFRESH_ON = False
+    return HttpResponse(REFRESH_ON)
+"""
+
+MEDIA_PATH = settings.MEDIA_URL[1:]     # Remove / at the beginning
+MEDIA_ROOT = settings.MEDIA_ROOT
+# REFRESH_ON = False
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+firebaseConfig = {
+                    "apiKey": env("FIREBASE_API_KEY"),
+                    "authDomain": "filebucketapp.firebaseapp.com",
+                    "projectId": "filebucketapp",
+                    "storageBucket": "filebucketapp.appspot.com",
+                    "messagingSenderId": "602108448767",
+                    "appId": "1:602108448767:web:b210cf7a7caa3bf963a1ca",
+                    "serviceAccount": env("GOOGLE_APPLICATION_CREDENTIALS"),
+                    "databaseURL": ""
+                 }
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+storage = firebase.storage()
+bucket = storage.bucket
---
+++ static/js/main.js
@@ -0,0 +1,241 @@
+const textarea = document.getElementById("user-text");
+const clearBtn = document.getElementById("clear");
+const copyBtn = document.getElementById("copy");
+const fileChooseBtn = document.getElementById("file-button");
+const fileChoose = document.getElementById("file-choose");
+const fileForm = document.getElementById("file-form");
+const fileUploadBtn = document.getElementById("file-button-upload");
+const fileDownloadBtn = document.getElementById("file-button-download");
+const fileDownloadLabel = document.getElementById("file-download-label");
+
+
+let mac = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
+
+if (mac) {
+    fileChooseBtn.innerText = "⬆ Choose File";
+    fileDownloadBtn.innerText = "⬇ Download";
+}else {
+    fileChooseBtn.innerText = "⮬ Choose File";
+    fileDownloadBtn.innerText = "⮮ Download";
+}
+
+textarea.addEventListener("keydown", (e) => {
+    var key = e.key || e.keyCode;
+    if (key === "Tab") {
+        e.preventDefault();
+        if (e.shiftKey) {
+            // SHIFT TAB PRESSED
+            let startPos = textarea.selectionStart;
+            textarea.setRangeText('', startPos - 4, startPos, 'end');
+        }
+        else {
+            // TAB PRESSED
+            let startPos = textarea.selectionStart;
+            textarea.setRangeText('    ', startPos, startPos, 'end');
+        }
+    }
+});
+
+clearBtn.addEventListener("click", () => {
+    textarea.value = "";
+});
+
+copyBtn.addEventListener("click", () => {
+    textarea.select();
+    textarea.setSelectionRange(0, 9999);
+    document.execCommand("copy");
+    textarea.setSelectionRange(0, 0);
+});
+
+fileChooseBtn.addEventListener("click", () => {
+    fileChoose.click();
+});
+
+fileChoose.addEventListener("change", (e) => {
+    e.preventDefault();
+    fileUploadBtn.click();
+});
+
+let labelData = fileDownloadLabel.getAttribute("value");
+if (labelData !== "") {
+    fileDownloadLabel.innerText = labelData;
+    fileDownloadLabel.style.display = "block";
+    fileDownloadBtn.style.display = "inline";
+}
+
+// Posible issues, file names with. and non image files
+function downloadFile(url, name) {
+    console.log(url);
+    fetch(url).then(async (res) => {
+        const dfile = await res.blob();
+        var blobURL = window.URL && window.URL.createObjectURL ?
+            window.URL.createObjectURL(dfile) :
+            window.webkitURL.createObjectURL(dfile);
+        var tempLink = document.createElement("a");
+        tempLink.style.display = "none";
+        tempLink.href = blobURL;
+        tempLink.setAttribute("download", name);
+
+        if (typeof tempLink.download == "undefined") {
+            tempLink.setAttribute("target", "_blank");
+        }
+
+        document.body.appendChild(tempLink);
+        tempLink.click();
+
+        setTimeout(() => {
+            document.body.removeChild(tempLink);
+            window.URL.revokeObjectURL(blobURL);
+        }, 200);
+    });
+}
+
+
+// Trying to make screen refresh
+
+/*
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
+// Create ajax request that polls every 3 seconds
+async function poll() {
+    console.log("Polling")
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/refresh", true);
+    xhr.onreadystatechange = function () {
+        let data = xhr.responseText;
+        if (data === "True"){
+            console.log("Refreshing");
+            // make post request
+            xhr.open("POST", "/refresh/", true);
+            // set the x-csrf-token header
+            xhr.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
+            xhr.send();
+            window.location.reload();
+        }
+    };
+    xhr.send();
+    setTimeout(poll, 5000);
+};
+poll();
+
+*/
+
+
+
+/*
+ALL KEY CODES
+
+backspace           8
+tab                 9
+enter               13
+shift               16
+ctrl                17
+alt                 18
+pause/break         19
+caps lock           20
+escape              27
+page up             33
+page down           34
+end                 35
+home                36
+left arrow          37
+up arrow            38
+right arrow         39
+down arrow          40
+insert              45
+delete              46
+0                   48
+1                   49
+2                   50
+3                   51
+4                   52
+5                   53
+6                   54
+7                   55
+8                   56
+9                   57
+a                   65
+b                   66
+c                   67
+d                   68
+e                   69
+f                   70
+g                   71
+h                   72
+i                   73
+j                   74
+k                   75
+l                   76
+m                   77
+n                   78
+o                   79
+p                   80
+q                   81
+r                   82
+s                   83
+t                   84
+u                   85
+v                   86
+w                   87
+x                   88
+y                   89
+z                   90
+left window key     91
+right window key    92
+select key          93
+numpad 0            96
+numpad 1            97
+numpad 2            98
+numpad 3            99
+numpad 4            100
+numpad 5            101
+numpad 6            102
+numpad 7            103
+numpad 8            104
+numpad 9            105
+multiply            106
+add                 107
+subtract            109
+decimal point       110
+divide              111
+f1                  112
+f2                  113
+f3                  114
+f4                  115
+f5                  116
+f6                  117
+f7                  118
+f8                  119
+f9                  120
+f10                 121
+f11                 122
+f12                 123
+num lock            144
+scroll lock         145
+semi-colon          186
+equal sign          187
+comma               188
+dash                189
+period              190
+forward slash       191
+grave accent        192
+open bracket        219
+back slash          220
+close braket        221
+single quote        222
+*/
